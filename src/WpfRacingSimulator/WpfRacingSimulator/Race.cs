3c54b22ba922a49cc4c77f5f5ef38acc4613912e
AleksandrBelokurov <sasacom@mail.ru>
2021-09-09 21:53:04 +0700

    classes definition

---
+++ src/WpfRacingSimulator/WpfRacingSimulator/Race.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfRacingSimulator
+{
+    class Race
+    {
+        List<Vechle> vechles;
+        public Race()
+        {
+            vechles = new List<Vechle> { 
+                new Truck() { Speed = 50, DamageRand = 0.1, Weight = 1000 }
+                , new Car() { Speed = 90, DamageRand = 0.1, Passengers = 2 }
+                , new Moto() { Speed = 120, DamageRand = 0.1, IsSidecar = false }
+            };
+        }
+    }
+}
--- src/WpfRacingSimulator/WpfRacingSimulator/Vechle.cs
+++ src/WpfRacingSimulator/WpfRacingSimulator/Vechle.cs
@@ -6,11 +6,102 @@ using System.Threading.Tasks;
 
 namespace WpfRacingSimulator
 {
-    class Vechle
+    abstract class Vechle
     {
-        public void Doit(MainWindow mainPage)
+        int speed_;
+        public int Speed
         {
-            mainPage.MyDisplayValue = "there";
+            get => speed_;
+            set
+            {
+                if ((value > 0) && (value < 180))
+                {
+                    speed_ = value;
+                }
+            }
+        }
+        double damageRand_;
+        public double DamageRand
+        {
+            get => damageRand_;
+            set
+            {
+                if ((value > 0) && (value < 1))
+                {
+                    damageRand_ = value;
+                }
+            }
+        }
+        int distance_;
+        public int Distance
+        {
+            get => distance_;
+            set
+            {
+                if ((value > 0) && (value < 1000))
+                {
+                    distance_ = value;
+                }
+            }
+        }
+        int damageCount_;
+        int damageCountInit_ = 5;
+        public void Run() 
+        {
+            Distance -= speed_;
+        }
+        public void Damage() 
+        {
+            var rand = new Random();
+            if (damageCount_ > 0)
+            {
+                --damageCount_;
+            } else if (rand.NextDouble() < damageRand_)
+            {
+                damageCount_ = damageCountInit_;
+            }
+        }
+    }
+    class Truck : Vechle
+    {
+        int weight_;
+        public int Weight
+        {
+            get => weight_;
+            set
+            {
+                if ((value >= 0) && (value < 5000))
+                {
+                    weight_ = value;
+                }
+            }
+        }
+    }
+    class Car : Vechle
+    {
+        int passengers_;
+        public int Passengers
+        {
+            get => passengers_;
+            set
+            {
+                if ((value >= 0) && (value < 4))
+                {
+                    passengers_ = value;
+                }
+            }
+        }
+    }
+    class Moto : Vechle
+    {
+        bool isSidecar_;
+        public bool IsSidecar
+        {
+            get => isSidecar_;
+            set
+            {
+                isSidecar_ = value;
+            }
         }
     }
 }
